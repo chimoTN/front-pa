3943c0b7ca8cb50a89f6bf7e668e692387b7fced
samiraseddar <seddar20191@gmail.com>
2024-11-14 20:00:27 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import { PrimeReactProvider, PrimeReactContext} from 'primereact/api';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
